c379d9cbbcfa414908485ed5c34771f180e6c7a9
rmcwhorter <ryan@superfocus.ai>
2024-03-05 21:33:53 -0500

    now token

--- src/client.rs
+++ src/client.rs
@@ -148,7 +148,7 @@ impl Client {
 
         let fut = if let Some(ref token) = self.jwt_token {
             println!("IN WRITE POINTS, WITH A TOKEN...");
-            self.client.post(url).body(line).header("Authorization", format!("Basic {token}")).send()
+            self.client.post(url).body(line).header("Authorization", format!("Token {token}")).send()
             //builder = builder.bearer_auth(token);
         } else {
             self.client.post(url).body(line).send()
